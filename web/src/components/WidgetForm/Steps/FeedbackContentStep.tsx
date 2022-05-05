c9d80318994494e6c4100f189a9025b5356901eb
Carlos <domingues_santos@hotmail.com>
2022-05-05 15:27:10 -0300

    finalizando aula 2

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,16 +1,31 @@
 
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
+
+
 
 interface FeedbackContentStepProps{
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: ()=>void;
+    onFeedbackSent:()=>void;
 }
 
-export function FeedbackContentStep({feedbackType,onFeedbackRestartRequested}:FeedbackContentStepProps){
+export function FeedbackContentStep({feedbackType,onFeedbackRestartRequested,onFeedbackSent}:FeedbackContentStepProps){
     
+    const[screenshot, setScreenshot] = useState<string | null>(null)
+    const [comment, setComment] = useState('');
     const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault();
+        console.log({screenshot,comment})
+
+        onFeedbackSent()
+    }
+ 
     return(
         <>
         <header>
@@ -28,12 +43,32 @@ export function FeedbackContentStep({feedbackType,onFeedbackRestartRequested}:Fe
         <CloseButton />
         </header>
 
-    <form  className="my-4 w-full">
+     <form  onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea 
-        className="min-w-[304] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-1 border-zinc-600 bg-transparent rounded-md"
-          placeholder="Conte com detalhes o que está acontecendo"
+        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-1 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparente scrollbar-thin"
+        placeholder="Conte com detalhes o que está acontecendo"
+        onChange={event =>setComment(event.target.value)}
+
         /> 
-    </form>
+
+
+
+        <footer className="flex gap-2 mt-2">
+
+        <ScreenshotButton
+        screenshot = {screenshot}
+        onScreenshotTook = {setScreenshot}
+        />
+
+            <button
+            type="submit"
+            disabled ={comment.length ===0}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+            >
+                Enviar feedback
+            </button>
+        </footer>
+    </form> 
             </>
     )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,8 +7,13 @@ module.exports = {
           300: '#996DFF',
           500: '#8257e6',
         }
+      }, 
+      borderRadius:{
+        md: '4px'
       }
     },
   },
-  plugins: [require('@tailwindcss/forms'),],
+  plugins: [require('@tailwindcss/forms'),
+            require('tailwind-scrollbar'),
+],
 }
